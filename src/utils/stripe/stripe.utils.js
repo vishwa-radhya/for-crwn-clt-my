ca27e271602b3b2cd99e19180c366b79a643ef23
vishwa-radhya <vishuvishwa735@gmail.com>
2024-03-01 09:37:21 +0530

    fixing things

--- src/utils/stripe/stripe.utils.js
+++ src/utils/stripe/stripe.utils.js
@@ -4,5 +4,5 @@ import { loadStripe } from "@stripe/stripe-js";
 
 // we need to pass publishiable key
 export const stripePromise = loadStripe(
-  `$ {process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY}`
+  process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY
  );

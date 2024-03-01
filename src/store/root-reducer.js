0540fc80dcb1231303ad51993dbd05ce5f780869
vishwa-radhya <vishuvishwa735@gmail.com>
2024-03-01 19:33:18 +0530

    typescript migration

--- src/store/root-reducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import { categoriesReducer } from './categories/category.reducer';
-import { userReducer } from './user/user-reducer';
-import { cartReducer } from './cart/cart.reducer';
-export const rootReducer = combineReducers({
-  user: userReducer,
-  categories:categoriesReducer,
-  cart:cartReducer,
-});
--- src/utils/reducer/reducer.utils.js
+++
@@ -1,6 +0,0 @@
- export const createAction =(type,payload)=>({type,payload});
-
-//reducers gives a new object everytime where we need to 
-//type them as object instead we can use a helper 
-//function which takes those args and returs 
-//a new boject
